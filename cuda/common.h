e3e01c8d454630c9217e98a518d87d9203281b69
abdullahejazjanjua <abdullahejaz822@gmail.com>
2026-04-12 00:39:42 +0500

    made small chanegs

--- cuda/common.h
+++ cuda/common.h
@@ -1,7 +1,7 @@
 #include <cuda_runtime.h>
 #include <iostream>
 
-#define CHECK_CUDA(code)                                                       \
+#define CUDA_CHECK(code)                                                       \
   do {                                                                         \
     if ((code) != cudaSuccess) {                                               \
       std::cerr << "GPU ERROR in " << __FILE__ << ":" << __LINE__              \
@@ -9,3 +9,8 @@
       exit(code);                                                              \
     }                                                                          \
   } while (0)
+
+
+int cdiv(int size, int block_size) {
+    return (size + block_size - 1) / block_size;
+}

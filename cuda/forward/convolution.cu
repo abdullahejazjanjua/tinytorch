e3a1e9b73cfc12d183793059b131e042dccd9a16
abdullahejazjanjua <abdullahejaz822@gmail.com>
2026-04-12 02:04:36 +0500

    extended conv to arbitary batch size

--- cuda/forward/convolution.cu
+++ cuda/forward/convolution.cu
@@ -16,7 +16,7 @@
 __global__ void conv2d_kernel(float *in, 
                               float *filter, 
                               float *out, 
-                              int h_in, int w_in, int h_out, int w_out, int num_channels,
+                              int batch_size, int h_in, int w_in, int h_out, int w_out, int num_channels,
                               int num_filters, int filter_size,
                               int pad_h, int pad_w, int stride
                             ) 
@@ -27,30 +27,41 @@ __global__ void conv2d_kernel(float *in,
 
     if (filter_k < num_filters && row < h_out && col < w_out)
     {
-        float val = 0.0f;
-        for (int c = 0; c < num_channels; c++) 
+        for (int bs = 0; bs < batch_size; bs++)
         {
-            for (int f_i = 0; f_i < filter_size; f_i++)
+            float val = 0.0f;
+            for (int c = 0; c < num_channels; c++) 
             {
-                for (int f_j = 0; f_j < filter_size; f_j++)
+                for (int f_i = 0; f_i < filter_size; f_i++)
                 {
-                    int row_hat = row + f_i - pad_h;
-                    int col_hat = col + f_j - pad_w;
-                    if (row_hat >= 0 && row_hat < h_in && col_hat >= 0 && col_hat < w_in)
-                        val += in[c * (h_in * w_in) + row_hat * w_in + col_hat] * 
-                            filter[filter_k * (num_channels * filter_size * filter_size) + 
-                                c * (filter_size * filter_size) + (f_i * filter_size) + f_j];
+                    for (int f_j = 0; f_j < filter_size; f_j++)
+                    {
+                        int row_hat = row + f_i - pad_h;
+                        int col_hat = col + f_j - pad_w;
+                        if (row_hat >= 0 && row_hat < h_in && col_hat >= 0 && col_hat < w_in)
+                            val += in[
+                                    bs * (num_channels * h_in * w_in) + 
+                                    c * (h_in * w_in) + row_hat * w_in + 
+                                    col_hat
+                                ] 
+                                     * 
+                                filter[
+                                    filter_k * (num_channels * filter_size * filter_size) + 
+                                    c * (filter_size * filter_size) + (f_i * filter_size) + 
+                                    f_j
+                                ];
+                    }
                 }
             }
+            out[bs * (num_filters * h_out * w_out) + filter_k * (h_out * w_out) + row * w_out + col] = val;
         }
-        out[filter_k * (h_out * w_out) + row * w_out + col] = val;
     }
 }
 
 void conv2d_forward_pass(float *in_h, 
                         float *filter_h, 
                         float *out_h, 
-                        int h_in, int w_in, int num_channels,
+                        int batch_size, int h_in, int w_in, int num_channels,
                         int num_filters, int filter_size, 
                         int padding, int stride
                     )
@@ -77,37 +88,41 @@ void conv2d_forward_pass(float *in_h,
         pad_w = (filter_size - 1) / 2;
     }
 
-    CUDA_CHECK( cudaMalloc((void**)&in_d, (num_channels * h_in * w_in * sizeof(float))) );
-    CUDA_CHECK( cudaMalloc((void**)&out_d, (num_filters * h_out * w_out * sizeof(float))) );
+    CUDA_CHECK( cudaMalloc((void**)&in_d, (batch_size * num_channels * h_in * w_in * sizeof(float))) );
+    CUDA_CHECK( cudaMalloc((void**)&out_d, (batch_size * num_filters * h_out * w_out * sizeof(float))) );
     CUDA_CHECK( cudaMalloc((void**)&filter_d, (num_filters * num_channels * filter_size * filter_size * sizeof(float)) ) );
 
-    CUDA_CHECK( cudaMemcpy(in_d, in_h, (num_channels * h_in * w_in * sizeof(float)), cudaMemcpyHostToDevice) );
+    CUDA_CHECK( cudaMemcpy(in_d, in_h, (batch_size * num_channels * h_in * w_in * sizeof(float)), cudaMemcpyHostToDevice) );
     CUDA_CHECK( cudaMemcpy(filter_d, filter_h, (num_filters * num_channels * filter_size * filter_size * sizeof(float)), cudaMemcpyHostToDevice) );
 
     dim3 dimBlock(32, 32, 1);
     dim3 dimGrid(cdiv(h_out, 32), cdiv(w_out, 32), num_filters);
 
-    conv2d_kernel<<<dimGrid, dimBlock>>>(in_d, filter_d, out_d, h_in, w_in, h_out, w_out, num_channels, num_filters, filter_size, pad_h, pad_w, stride);
+    conv2d_kernel<<<dimGrid, dimBlock>>>(in_d, filter_d, out_d, batch_size, h_in, w_in, h_out, w_out, num_channels, num_filters, filter_size, pad_h, pad_w, stride);
     CUDA_CHECK(cudaGetLastError());
     
-    CUDA_CHECK(cudaMemcpy(out_h, out_d, (num_filters * h_out * w_out * sizeof(float)), cudaMemcpyDeviceToHost));
+    CUDA_CHECK(cudaMemcpy(out_h, out_d, (batch_size * num_filters * h_out * w_out * sizeof(float)), cudaMemcpyDeviceToHost));
     
     cudaFree(in_d);
     cudaFree(out_d);
     cudaFree(filter_d);
 }
-int main() {
+
+
+int main() 
+{
+    int batch_size = 2;
     int h_in = 5;
     int w_in = 5;
     int num_channels = 3;
-    int num_filters = 8;
+    int num_filters = 2; 
     int filter_size = 3;
     int stride = 1;
 
-    float *in_h = (float*)malloc(num_channels * h_in * w_in * sizeof(float));
+    float *in_h = (float*)malloc(batch_size * num_channels * h_in * w_in * sizeof(float));
     float *filter_h = (float*)malloc(num_filters * num_channels * filter_size * filter_size * sizeof(float));
 
-    for (int i = 0; i < num_channels * h_in * w_in; i++) {
+    for (int i = 0; i < batch_size * num_channels * h_in * w_in; i++) {
         in_h[i] = 1.0f;
     }
 
@@ -116,13 +131,16 @@ int main() {
     }
 
     printf("Input Data:\n");
-    for (int c = 0; c < num_channels; c++) {
-        printf("Channel %d:\n", c);
-        for (int i = 0; i < h_in; i++) {
-            for (int j = 0; j < w_in; j++) {
-                printf("%f ", in_h[c * (h_in * w_in) + i * w_in + j]);
+    for (int bs = 0; bs < batch_size; bs++) {
+        printf("Batch %d:\n", bs);
+        for (int c = 0; c < num_channels; c++) {
+            printf("  Channel %d:\n", c);
+            for (int i = 0; i < h_in; i++) {
+                for (int j = 0; j < w_in; j++) {
+                    printf("%f ", in_h[bs * (num_channels * h_in * w_in) + c * (h_in * w_in) + i * w_in + j]);
+                }
+                printf("\n");
             }
-            printf("\n");
         }
     }
 
@@ -144,18 +162,21 @@ int main() {
     int padding_valid = 0;
     int h_out_valid = h_in - filter_size + 1;
     int w_out_valid = w_in - filter_size + 1;
-    float *out_h_valid = (float*)malloc(num_filters * h_out_valid * w_out_valid * sizeof(float));
+    float *out_h_valid = (float*)malloc(batch_size * num_filters * h_out_valid * w_out_valid * sizeof(float));
 
-    conv2d_forward_pass(in_h, filter_h, out_h_valid, h_in, w_in, num_channels, num_filters, filter_size, padding_valid, stride);
+    conv2d_forward_pass(in_h, filter_h, out_h_valid, batch_size, h_in, w_in, num_channels, num_filters, filter_size, padding_valid, stride);
     
     printf("\nOutput Data (Padding = 0):\n");
-    for (int k = 0; k < num_filters; k++) {
-        printf("Filter %d:\n", k);
-        for (int i = 0; i < h_out_valid; i++) {
-            for (int j = 0; j < w_out_valid; j++) {
-                printf("%f ", out_h_valid[k * (h_out_valid * w_out_valid) + i * w_out_valid + j]);
+    for (int bs = 0; bs < batch_size; bs++) {
+        printf("Batch %d:\n", bs);
+        for (int k = 0; k < num_filters; k++) {
+            printf("  Filter %d:\n", k);
+            for (int i = 0; i < h_out_valid; i++) {
+                for (int j = 0; j < w_out_valid; j++) {
+                    printf("%f ", out_h_valid[bs * (num_filters * h_out_valid * w_out_valid) + k * (h_out_valid * w_out_valid) + i * w_out_valid + j]);
+                }
+                printf("\n");
             }
-            printf("\n");
         }
     }
     free(out_h_valid);
@@ -164,18 +185,21 @@ int main() {
     int padding_same = 1;
     int h_out_same = h_in;
     int w_out_same = w_in;
-    float *out_h_same = (float*)malloc(num_filters * h_out_same * w_out_same * sizeof(float));
+    float *out_h_same = (float*)malloc(batch_size * num_filters * h_out_same * w_out_same * sizeof(float));
 
-    conv2d_forward_pass(in_h, filter_h, out_h_same, h_in, w_in, num_channels, num_filters, filter_size, padding_same, stride);
+    conv2d_forward_pass(in_h, filter_h, out_h_same, batch_size, h_in, w_in, num_channels, num_filters, filter_size, padding_same, stride);
     
     printf("\nOutput Data (Padding = 1):\n");
-    for (int k = 0; k < num_filters; k++) {
-        printf("Filter %d:\n", k);
-        for (int i = 0; i < h_out_same; i++) {
-            for (int j = 0; j < w_out_same; j++) {
-                printf("%f ", out_h_same[k * (h_out_same * w_out_same) + i * w_out_same + j]);
+    for (int bs = 0; bs < batch_size; bs++) {
+        printf("Batch %d:\n", bs);
+        for (int k = 0; k < num_filters; k++) {
+            printf("  Filter %d:\n", k);
+            for (int i = 0; i < h_out_same; i++) {
+                for (int j = 0; j < w_out_same; j++) {
+                    printf("%f ", out_h_same[bs * (num_filters * h_out_same * w_out_same) + k * (h_out_same * w_out_same) + i * w_out_same + j]);
+                }
+                printf("\n");
             }
-            printf("\n");
         }
     }
     free(out_h_same);
